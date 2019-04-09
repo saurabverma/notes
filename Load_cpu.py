c407c2e7f2fb341ffd857da192b60ab2f93015d8
saurabverma <saurab_verma@i2r.a-star.edu.sg>
2019-04-09 09:51:34 +0800

    Added Ctrl+C functionality

--- Load_cpu.py
+++ Load_cpu.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
 '''
-This python program runs a fictional load on desired number of threads on the PC useful for PC examination under load.
+This python program runs a fictional load on desired number of threads on the PC, useful for PC examination under load.
 Run as './Load_cpu.py', after chmod set to u+x.
 '''
 
 from argparse import ArgumentParser
 from multiprocessing import Process
+from signal import signal, SIGINT
+
+shutdown = False
+
+def signal_handler(sig, frame):
+	global shutdown
+	shutdown = True
 
 def fibonacci(num):
 	n1 = 0
@@ -16,11 +23,15 @@ def fibonacci(num):
 		nth = n1 + n2
 		n1 = n2
 		n2 = nth
-		count += 1
+		if shutdown:
+			count = num
+		else:
+			count += 1
 	return nth
 
 if __name__ == '__main__':
 	
+	signal(SIGINT, signal_handler)
 	print("Press 'Ctrl+C' to exit\n")
 	
 	parser = ArgumentParser(description="CPU dummy load")
